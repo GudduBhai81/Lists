4df49b1d78b0c629383651e190364518b63f4f1e
GudduBhai81 <306botala@gmail.com>
2024-11-05 21:11:53 +0500

    Add files via upload

---
+++ List.py
@@ -0,0 +1,12 @@
+empty_list = []
+print()
+
+number = [1, 4, 6, 8, 10]
+print(number[1:4])
+
+triples = [1, 2, 3] * 10
+print(triples)
+
+aList = [102, 232, 2323, 2324, 2112]
+aList = aList[::-1]
+print(aList,"\n")
